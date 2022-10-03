34a1661304396b8f3f595fb8f577dc54b594cf5b
Shawn Cochran <shawn.cochran31@gmail.com>
2022-10-03 16:19:04 -0600

    background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@
 import * as React from 'react'
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Box } from '@chakra-ui/react'
 import Nav from './components/Nav'
+import background from './assets/DesertLights.jpg'
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
+      <Box
+        bgImage={background}
+        bgPosition='center'
+        minH='100vh'
+        width='100%'
+      >
       <Nav />
+      </Box>
     </ChakraProvider>
   )
 }
